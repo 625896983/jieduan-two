2ebda072acc20a27baa9961aecdfb81bbb452c01
625896983 <625896983@qq.com>
2019-12-10 13:05:33 +0800

    12/10中午

--- server/scss.js
+++ server/scss.js
@@ -4,10 +4,19 @@
 	"use strict";
 	class Scss{
 		constructor(){
-			this.type=0;
+			
+		this.type=""
 		this.black=document.getElementById("black");
 		this.red=document.getElementById("red");
+			this.beimei=document.getElementById("beimei");
+			this.ouzhou=document.getElementById("ouzhou");
+			this.rihan=document.getElementById("rihan");
+			this.cjdl=document.getElementById("cjdl");
+			this.cjzh=document.getElementById("cjzh");
+			this.cjmm=document.getElementById("cjmm");
 		this.xiamore=document.getElementById("xiamore");
+			this.tou1=document.getElementById("tou1");
+			this.tou2=document.getElementById("tou2");
 			this.floor=document.getElementById("floor");
 			this.xxx=document.getElementById("xxx");
 			this.search=document.getElementById("search");
@@ -16,46 +25,193 @@
 		this.sbox1=document.getElementsByClassName("sbox1");
 		this.sbox2=document.getElementsByClassName("sbox2");
 		this.zz=document.getElementsByClassName("zz");	
-	this.addEvent();	
+		this.dd=document.getElementsByClassName("dd");	
+		this.addEvent();	
 		}
 	
 	
   addEvent(){
-	  var that=this
+	  var that=this;
 	 console.log(12);
-	  this.zz[0].onclick=function(){
+	 
+	  console.log(this.type)
+	  
+	  window.onload=function(){
+		   console.log(getCookie("onoff"))
+		  if(getCookie("onoff")){
+			  console.log(1)
+			  that.type=getCookie("onoff");
+		  }else{
+			  console.log(2)
+			  that.type=0
+			   console.log(that.type);
+		  }
+		  	if(that.type==0){
+		that.tou1.innerHTML="登录";
+		that.tou2.innerHTML="注册";
+				
+				
+				
+		that.dd[0].onclick=function(){
 		that.black.style.display="block"  
 	  }
-	  this.xxx.onclick=function(){
-		that.black.style.display="none"  
+	  that.zz[0].onclick=function(){
+		location.href="注册.html"
 	  }
-	if(this.type==0){
-		this.red.onclick=function(){
+		that.red.onclick=function(){
 		alert("请先登录");
 	}
-		this.search.onclick=function(){
+		that.search.onclick=function(){
 		alert("请先登录");
 	}
 		
-					this.xiamore.onclick=function(){
+					that.xiamore.onclick=function(){
 					alert("请先登录");
 				}
-			for(let i=0;i<this.sbox.length;i++){
-					this.sbox[i].onclick=function(){
+			for(let i=0;i<that.sbox.length;i++){
+					that.sbox[i].onclick=function(){
 					alert("请先登录");
 				}}
-				for(let i=0;i<this.sbox1.length;i++){
-					this.sbox1[i].onclick=function(){
+				for(let i=0;i<that.sbox1.length;i++){
+					that.sbox1[i].onclick=function(){
 					alert("请先登录");
 				}}
-				
+			that.cjdl.onclick=function(){
+				that.u=that.cjzh.value;
+					that.p=that.cjmm.value;
+					that.getMsg();
+				console.log("cjdl")
+			}
+	}else if(that.type==1){
+		that.tou1.innerHTML="已登录";
+		that.tou2.innerHTML="退出登录";
+		that.zz[0].onclick=function(){
+		setCookie("onoff",0);
+			location.href="index.html"
 	}
-	for(let i=0;i<this.sbox2.length;i++){
+	that.red.onclick=function(){location.href="list.html"};
+				for(let i=0;i<that.sbox.length;i++){
+					that.sbox[i].onclick=function(){
+						console.log(1)
+						location.href="list.html"}
+				}
+				for(let i=0;i<that.sbox1.length;i++){
+					that.sbox1[i].onclick=function(){
+						console.log(2)
+						location.href="list.html"}
+				}
+	
+}	
+	  }
+	 for(let i=0;i<this.sbox2.length;i++){
 					this.sbox2[i].onclick=function(){
 					alert("直播已结束");
 				}}
-	
-}
+	  this.xxx.onclick=function(){
+		that.black.style.display="none"  
+	  }
+	  this.ouzhou.onclick=function(){
+		 for(let i=0;i<that.sbox.length;i++){
+					that.sbox[i].children[0].src="images/vovq.jpg";
+			 that.sbox[i].children[1].innerHTML="VOVA";
+			 that.sbox[i].children[2].innerHTML="货通全球，让世界感受欧洲制造"
+				}
+		  that.beimei.className="";
+		  that.rihan.className="";
+		  that.ouzhou.className="now"
+	  }
+	  this.beimei.onclick=function(){
+		 for(let i=0;i<that.sbox.length;i++){
+					that.sbox[i].children[0].src="images/亚马逊1.jpg";
+			 		that.sbox[i].children[1].innerHTML="亚马逊";
+			 		that.sbox[i].children[2].innerHTML="打造优质。。。，亚马逊亚马逊亚马逊妈妈妈妈吗讯"
+				}
+		  that.beimei.className="now";
+		  that.rihan.className="";
+		  that.ouzhou.className=""
+	  }
+	  this.rihan.onclick=function(){
+		 for(let i=0;i<that.sbox.length;i++){
+					that.sbox[i].children[0].src="images/乐天.png";
+			 that.sbox[i].children[1].innerHTML="日本乐天";
+			 that.sbox[i].children[2].innerHTML="日本知名的B2C网购平台，有互联网超市之美称"
+				}
+		  that.beimei.className="";
+		  that.rihan.className="now";
+		  that.ouzhou.className=""
+	  }
+	  }
+//	if(this.type==0){
+//		this.tou1.innerHTML="登录";
+//		this.tou2.innerHTML="注册";
+//		this.dd[0].onclick=function(){
+//		that.black.style.display="block"  
+//	  }
+//	  this.zz[0].onclick=function(){
+//		location.href="注册.html"
+//	  }
+//		this.red.onclick=function(){
+//		alert("请先登录");
+//	}
+//		this.search.onclick=function(){
+//		alert("请先登录");
+//	}
+//		
+//					this.xiamore.onclick=function(){
+//					alert("请先登录");
+//				}
+//			for(let i=0;i<this.sbox.length;i++){
+//					this.sbox[i].onclick=function(){
+//					alert("请先登录");
+//				}}
+//				for(let i=0;i<this.sbox1.length;i++){
+//					this.sbox1[i].onclick=function(){
+//					alert("请先登录");
+//				}}
+//				
+//	}else if(this.type==1){
+//		this.tou1.innerHTML="已登录";
+//		this.tou2.innerHTML="退出登录";
+//		this.zz[0].onclick=function(){
+//		setCookie("onoff",1);
+//			location.href="index.html"
+//	}
+//	for(let i=0;i<this.sbox2.length;i++){
+//					this.sbox2[i].onclick=function(){
+//					alert("直播已结束");
+//				}}
+//	
+//}
+		
+		getMsg(){
+			var type1=0;
+				this.msg=getCookie("userMsg")?JSON.parse(getCookie("userMsg")):[];
+				
+				for(var i=0;i<this.msg.length;i++){
+					
+					if(this.u==this.msg[i].user && this.p == this.msg[i].pass){
+						setCookie("onoff",1)
+					   location.href="index.html";
+						this.msg[i].condition=1;
+						
+						setCookie("userMsg",JSON.stringify(this.msg));
+						type1=1;
+						
+						
+					   }else if(this.u==this.msg[i].user && this.p !== this.msg[i].pass){
+						  
+						   alert("密码错误"); 
+						    type1=2;
+					   }
+					
+					
+				}
+				if(type1==0){
+						alert("用户名尚未注册")
+					}
 		}
+		
+		
+	}
 	new Scss();
 //})();
--- server/注册.js
+++ server/注册.js
@@ -4,6 +4,9 @@
 class Zhuce{
 	constructor(){
 		this.password=document.getElementById("password");
+		this.account=document.getElementById("account");
+		this.ljzc=document.getElementById("ljzc");
+		this.agree=document.getElementById("agree");
 		this.reg1=/^\w{6,12}$/;
 		this.addEvent();
 	}
@@ -11,6 +14,9 @@ class Zhuce{
 		
 		
 		var that=this;
+		window.onload=function(){
+			this.password.value="";
+		}
 		this.password.onblur=function(){
 		console.log(that.password.value)
 			if(!that.reg1.test(that.password.value)){
@@ -22,6 +28,54 @@ class Zhuce{
 				}
 			}
 		}
+		this.zhuCe();
 	}
+	zhuCe(){
+		var that=this;
+		this.ljzc.onclick=function(){
+			if(that.reg1.test(that.password.value) && that.agree.checked==true){
+				that.n=that.account.value;
+				that.p=that.password.value;
+				that.setMsg();
+				
+				
+				
+				
+			}else{
+				alert("请正确输入账号和密码，并同意用户服务协议")
+			}
+		}
+	}
+	setMsg(){
+		this.msg=getCookie("userMsg")?JSON.parse(getCookie("userMsg")):[];
+				//查重
+				console.log(this.msg)
+				var type=this.msg.some((val,idx)=>{
+					return val.user===this.n
+					
+				})
+				if(type){
+					//重了
+					
+					alert("用户名重复");
+				}else{
+					this.msg.push({
+						user:this.n,
+						pass:this.p,
+						condition:0
+					})
+					this.Success()	
+					
+				}
+				setCookie("userMsg",JSON.stringify(this.msg));
+		
+	}
+	Success(){
+		setCookie("onoff",1);
+				alert("注册成功 确定后将自动登录");
+				setTimeout(()=>{
+					location.href="index.html"
+				},2000)
+			}
 }
 new Zhuce();
